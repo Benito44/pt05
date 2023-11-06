7ba62ce18f9526d251d23aa042edd48a2c1e2a40
BenitoMF <b.martinez2@sapalomera.cat>
2023-11-06 12:16:16 +0100

    HybridAuth

--- configuracion.php
+++ configuracion.php
@@ -1,4 +1,5 @@
 <?php
+/*
   require_once 'vendor/autoload.php';
 
   $clientID = '532009720343-mupdo12pg1nbdmngk9kdev76p47ukc5t.apps.googleusercontent.com';
@@ -12,4 +13,22 @@
   $client->setRedirectUri($redirectUri);
   $client->addScope("email");
   $client->addScope("profile");
-?>
+*/
+
+require_once 'vendor/autoload.php';
+ 
+$config = [
+    'callback' => 'http://localhost/Backend/UF2/pt05/Controlador/index.logat.php',
+    'keys'     => [
+                    'id' => '532009720343-mupdo12pg1nbdmngk9kdev76p47ukc5t.apps.googleusercontent.com',
+                    'secret' => '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'
+                ],
+    'scope'    => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+    'authorize_url_parameters' => [
+            'approval_prompt' => 'force', // to pass only when you need to acquire a new refresh token.
+            'access_type' => 'offline'
+    ]
+];
+ 
+$adapter = new Hybridauth\Provider\Google( $config );
+?>  
